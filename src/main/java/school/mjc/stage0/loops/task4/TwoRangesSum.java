68013afbacef129410d7c95b156a8d0ba18f3b71
Madara <ranel28021998@gmail.com>
2022-12-25 22:05:11 +0300

    Fixed TwoRangesSum.. I think so

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,10 +1,7 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
-    public static void main(String[] args) {
-        printSumOfTwoRanges(10, 10);
-    }
-    public static void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
         if (numberToSkip > lastInRow) {
             System.out.println("number to skip is bugger then the last");
         } else if (lastInRow < 0) {
@@ -12,13 +9,16 @@ public class TwoRangesSum {
         } else {
             int countedSum = 0;
             int skippedSum = 0;
-            for (int i = 0; i <= lastInRow; i++) {
-                countedSum += i;
+            int i = 1;
+            for (; i <= numberToSkip; i++) {
+                skippedSum += i;
                 if (i == numberToSkip) {
-                    for (; i < lastInRow; i++) {
-                        skippedSum += i;
-                    }
+                    i++;
+                    break;
                 }
+            }
+            for (; i <= lastInRow; i++) {
+                countedSum += i;
             }
             System.out.println("skipped sum is " + skippedSum);
             System.out.println("counted sum is " + countedSum);
